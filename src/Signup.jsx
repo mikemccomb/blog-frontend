b16db52318ea1c865cf43ce01906cec8be185097
mikemccomb <mikemccomb@gmail.com>
2023-04-17 13:41:13 -0500

    Connects Signup form to backend

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,21 +1,40 @@
+import axios from "axios";
+
 export function Signup() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+    const params = new FormData(event.target);
+
+    axios
+      .post("http://localhost:3000/users.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.reset();
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
+
   return (
     <div>
       <h2>Signup Form</h2>
       <div id="signup">
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
-            Name: <input type="text" />
+            Name: <input type="text" name="name" />
           </div>
           <div>
-            Email: <input type="email" />
+            Email: <input type="email" name="email" />
           </div>
           <div>
-            Password: <input type="password" />
+            Password: <input type="password" name="password" />
           </div>
           <div>
-            Confirm Password: <input type="password" />
+            Confirm Password: <input type="password" name="password_confirmation" />
           </div>
+          <button type="submit">Finish Signup</button>
         </form>
       </div>
     </div>
