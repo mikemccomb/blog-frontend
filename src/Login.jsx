4c9fb69fa6f0363ef11cba6f36fa6a6af3426234
mikemccomb <mikemccomb@gmail.com>
2023-04-17 14:06:48 -0500

    Revises signup and login components

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,35 +1,50 @@
 import axios from "axios";
+import { useState } from "react";
+
+const jwt = localStorage.getItem("jwt");
+if (jwt) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
+}
 
 export function Login() {
+  const [errors, setErrors] = useState([]);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("handleSubmit");
+    setErrors([]);
     const params = new FormData(event.target);
-
     axios
       .post("http://localhost:3000/sessions.json", params)
       .then((response) => {
         console.log(response.data);
+        axios.defaults.headers.common["Authorization"] = "Bearer " + response.data.jwt;
+        localStorage.setItem("jwt", response.data.jwt);
         event.target.reset();
+        window.location.href = "/"; // Change this to hide a modal, redirect to a specific page, etc.
       })
       .catch((error) => {
-        console.log(error.response.data.errors);
+        console.log(error.response);
+        setErrors(["Invalid email or password"]);
       });
   };
+
   return (
-    <div>
-      <h2>Log In</h2>
-      <div id="login">
-        <form onSubmit={handleSubmit}>
-          <div>
-            Email: <input type="email" name="email" />
-          </div>
-          <div>
-            Password: <input type="password" name="password_digest" />
-          </div>
-          <button type="submit">Login</button>
-        </form>
-      </div>
+    <div id="login">
+      <h1>Login</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          Email: <input name="email" type="email" />
+        </div>
+        <div>
+          Password: <input name="password" type="password" />
+        </div>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
