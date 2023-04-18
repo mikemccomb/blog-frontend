8f472cd8809cbce122421b0e552ecd50acc12919
mikemccomb <mikemccomb@gmail.com>
2023-04-18 11:40:20 -0500

    Connects new post form to backend

--- src/PostsNew.jsx
+++ src/PostsNew.jsx
@@ -1,13 +1,26 @@
+import axios from "axios";
+
 export function PostsNew() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleSubmit");
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/posts.json", params).then((response) => {
+      console.log(response.data);
+      event.target.reset();
+    });
+  };
+
   return (
     <div id="posts-new">
       <h1>New post</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="input-group mb-3">
           <span className="input-group-text" id="inputGroup-sizing-default">
             Title:
           </span>
           <input
+            name="title"
             type="text"
             className="form-control"
             aria-label="Sizing example input"
@@ -19,17 +32,24 @@ export function PostsNew() {
             Body:
           </span>
           <input
+            name="body"
             type="text"
             className="form-control"
             aria-label="Sizing example input"
             aria-describedby="inputGroup-sizing-default"
           />
         </div>
-        <div class="mb-3">
-          <label for="formFile" class="form-label">
-            Upload Image:
-          </label>
-          <input class="form-control" type="file" id="formFile" />
+        <div className="input-group mb-3">
+          <span className="input-group-text" id="inputGroup-sizing-default">
+            Image URL:
+          </span>
+          <input
+            name="image"
+            type="url"
+            className="form-control"
+            aria-label="Sizing example input"
+            aria-describedby="inputGroup-sizing-default"
+          />
         </div>
         <div>
           <input type="submit"></input>
