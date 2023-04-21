8e5e658e0af4f4b92d916ead5231b197ee52bbcf
mikemccomb <mikemccomb@gmail.com>
2023-04-21 13:30:47 -0500

    Adds search filter to index

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
+
 export function PostsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   console.log(props);
   return (
     <div id="posts-index">
-      {props.posts.map((post) => (
-        <div key={post.id} className="post">
-          <h2>{post.title}</h2>
-          <a href={`/posts/${post.id}`}>
-            <img src={post.image} alt={post.title} />
-          </a>
-          <br />
-          <div className="d-grid gap-2 col-6 mx-auto">
-            <button onClick={() => props.onShowPost(post)}>Edit post</button>
+      <div id="search-filter">
+        Search filter:{"  "}
+        <input type="search" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+      </div>
+      {props.posts
+        .filter((post) => post.body.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((post) => (
+          <div key={post.id} className="post">
+            <h2>{post.title}</h2>
+            <a href={`/posts/${post.id}`}>
+              <img src={post.image} alt={post.title} />
+            </a>
+            <br />
+            <div className="d-grid gap-2 col-6 mx-auto">
+              <button onClick={() => props.onShowPost(post)}>Edit post</button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
